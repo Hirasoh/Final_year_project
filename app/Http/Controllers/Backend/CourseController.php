23f5a1ecd9022130483ceb115487d0a3f9c112e3
HareemArsh <hareemkhawaja13@gmail.com>
2024-03-23 05:30:52 +0500

    first

---
+++ app/Http/Controllers/Backend/CourseController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\SubCategory;
+use App\Models\Course;
+use App\Models\Course_goal;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Auth; 
+use Carbon\Carbon;
+
+class CourseController extends Controller
+{
+    public function AllCourse(){
+ 
+      $id = Auth::user()->id;
+      $courses = Course::where('instructor_id',$id)->orderBy('id','desc')->get();
+        return view('instructor.courses.all_course',compact('courses'));
+
+    }// End Method 
+
+    public function AddCourse(){
+
+        $categories = Category::latest()->get();
+        return view('instructor.courses.add_course',compact('categories'));
+
+    }// End Method 
+
+
+    public function GetSubCategory($category_id){
+
+        $subcat = SubCategory::where('category_id',$category_id)->orderBy('subcategory_name','ASC')->get();
+        return json_encode($subcat);
+
+    }// End Method 
+
+    public function StoreCourse(Request $request){
+
+        $request->validate([
+            'video' => 'required|mimes:mp4|max:10000',
+        ]);
+
+        $image = $request->file('course_image');  
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(370,246)->save('upload/course/thambnail/'.$name_gen);
+        $save_url = 'upload/course/thambnail/'.$name_gen;
+
+        $video = $request->file('video');  
+        $videoName = time().'.'.$video->getClientOriginalExtension();
+        $video->move(public_path('upload/course/video/'),$videoName);
+        $save_video = 'upload/course/video/'.$videoName;
+
+        $course_id = Course::insertGetId([
+
+            'category_id' => $request->category_id,
+            'subcategory_id' => $request->subcategory_id,
+            'instructor_id' => Auth::user()->id,
+            'course_title' => $request->course_title,
+            'course_name' => $request->course_name,
+            'course_name_slug' => strtolower(str_replace(' ', '-', $request->course_name)),
+            'description' => $request->description,
+            'video' => $save_video,
+
+            'label' => $request->label,
+            'duration' => $request->duration,
+            'resources' => $request->resources,
+            'certificate' => $request->certificate,
+            'selling_price' => $request->selling_price,
+            'discount_price' => $request->discount_price,
+            'prerequisites' => $request->prerequisites,
+
+            'bestseller' => $request->bestseller,
+            'featured' => $request->featured,
+            'highestrated' => $request->highestrated,
+            'status' => 1,
+            'course_image' => $save_url,
+            'created_at' => Carbon::now(),
+
+        ]);
+
+        /// Course Goals Add Form 
+
+        $goles = Count($request->course_goals);
+        if ($goles != NULL) {
+            for ($i=0; $i < $goles; $i++) { 
+                $gcount = new Course_goal();
+                $gcount->course_id = $course_id;
+                $gcount->goal_name = $request->course_goals[$i];
+                $gcount->save();
+            }
+        }
+        /// End Course Goals Add Form 
+
+        $notification = array(
+            'message' => 'Course Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.course')->with($notification);  
+
+    }// End Method 
+
+
+
+
+} 
